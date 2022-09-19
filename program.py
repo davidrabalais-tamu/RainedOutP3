89d35e745516c47c88a28c16a53ab9d0d4d4d39a
davidrabalais-tamu <davidrabalais@tamu.edu>
2022-09-19 17:33:47 -0500

    Formatting instructions on program.py

--- program.py
+++ program.py
@@ -1,5 +1,5 @@
-print ('This is the program for Group 6 Project #3\r\n')
-print('If you have\'t already, please install tabulate with the following command before proceeding.\r\n\npip3 install tabulate\n')
+print ('This is the program for Group 6 Project #3')
+print('\nTo run the program for this project you will need to first perform the following steps:\n1. sudo apt update\n2. sudo apt upgrade\n3. sudo apt install python3-pip\n4. pip3 install tabulate\n')
 input('Press ENTER to continue or CTRL+C to quit...')
 print ('Downloading the log file...')
 import download_log

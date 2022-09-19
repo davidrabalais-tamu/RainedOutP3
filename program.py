18e9e3bf860c1e401009b507c8f1a5b94b81ddbf
davidrabalais-tamu <davidrabalais@tamu.edu>
2022-09-19 17:10:14 -0500

    Added verbiage to program.py to request that tabulate be installed before continuing

--- program.py
+++ program.py
@@ -1,4 +1,6 @@
-print ('This is the program for Group 6 Project #3')
+print ('This is the program for Group 6 Project #3\r\n')
+print('If you have\'t already, please install tabulate with the following command before proceeding.\r\n\npip3 install tabulate\n')
+input('Press ENTER to continue or CTRL+C to quit...')
 print ('Downloading the log file...')
 import download_log
 print ('\n \nParsing the log file and counting number of requests')
